4dbed57090153b291b669c8533ffb91d0fe65c61
royal-deer-wq <thaolinhhoabinh@gmail.com>
2026-04-01 01:06:22 +0700

    Refactor C code to C++ with improved input/output

--- bai 3.cpp
+++ bai 3.cpp
@@ -1,22 +1,30 @@
-#include<stdio.h>
-#include<conio.h>
-#include <math.h>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 int main() {
-	int i;
-	int n;
-	float x[200], tbc, tong=0; 
-	do {
-		printf("\n Nhap n = "); 
-		scanf("%d", &n);
-		} while ((n<1) || (n>200)); 
-	printf("\nNhap day: "); 
-	for (i=0;i<200; i++)
-		scanf("%f",&x[i]);
-	for (i=0; i<200; i++) 
-		tong+= x[i];
-	tbc=tong/100;
-	for (i=0; i<200;i++)
-		if (fabs (double (tbc-x[i]))>=1)
-			printf("46.2",x[i]);
-	getch();
+    int n;
+    float x[200], tbc, tong = 0;
+    do {
+        cout << "Nhap n (1 <= n <= 200): ";
+        cin >> n;
+    } while (n < 1 || n > 200);
+    cout << "Nhap day " << n << " so:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "x[" << i << "] = ";
+        cin >> x[i];
+    }
+    for (int i = 0; i < n; i++) {
+        tong += x[i];
+    }
+    tbc = tong / n;
+    cout << fixed << setprecision(2);
+    cout << "\nTrung binh cong = " << tbc << endl;
+    cout << "Cac phan tu thoa man dieu kien (|TBC - x[i]| >= 1): " << endl;
+    for (int i = 0; i < n; i++) {
+        if (abs(tbc - x[i]) >= 1.0) {
+            cout << setw(8) << x[i]; // Thay cho printf("%6.2f")
+        }
+    }
+    cout << endl;
 }
