417c88343d45a04790117eeb1a863f4bbd6c7e1a
royal-deer-wq <thaolinhhoabinh@gmail.com>
2026-04-01 01:10:58 +0700

    Refactor C++ code to use iostream and dynamic memory

--- bai 9.cpp
+++ bai 9.cpp
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int main() {
     int i, n, d = 0, tg;
     int *x;
-    printf("\nNhap so phan tu cua day: ");
-    scanf("%d", &n);
-    x = (int*) malloc(n * sizeof(int));
+    cout << "\nNhap so phan tu cua day: ";
+    cin >> n;
+    x = new int[n];
     for (i = 0; i < n; i++) {
-        printf("x[%d] = ", i);
-        scanf("%d", &x[i]);
+        cout << "x[" << i << "] = ";
+        cin >> x[i];
     }
-    printf("\nDay truoc sap xep:\n");
+    cout << "\nDay truoc sap xep:\n";
     for (i = 0; i < n; i++) {
-        printf("%d ", x[i]);
+        cout << x[i] << " ";
     }
     for (i = 0; i < n; i++) {
         if (x[i] % 2 != 0) {
@@ -22,10 +23,9 @@ int main() {
             d++;
         }
     }
-    printf("\nDay sau sap xep:\n");
+    cout << "\nDay sau sap xep:\n";
     for (i = 0; i < n; i++) {
-        printf("%d ", x[i]);
+        cout << x[i] << " ";
     }
-    free(x);
-    return 0;
+    delete[] x;
 }
