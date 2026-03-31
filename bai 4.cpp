5d67c80514c19031963ddb545e62a370298f7637
royal-deer-wq <thaolinhhoabinh@gmail.com>
2026-04-01 01:07:21 +0700

    Refactor sorting program to use C++ and STL

--- bai 4.cpp
+++ bai 4.cpp
@@ -1,21 +1,25 @@
-#include<stdio.h>
-#include<conio.h>
-#define n 200
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 200;
 int main() {
-	int day [n], i, j, tam;
-	printf("Nhap vao cac phan tu: "); 
-	for (i=0; i<n; i++)
-		scanf("%d", &day[i]);
-	for (i=0; i<n-1; i++) 
-		for (j=i+1; j<n; j++) 
-			if (day [j] < day [i])
-				{
-					tam = day [i];
-					day [i] = day [j];
-					day [j] = tam;
-				}
-	printf("\nSau khi sap xep: ");
-	for (i=0; i<n; i++) 
-		printf("%d ", day[i]);
-	getch();
+    int day[N], tam;
+    cout << "--- Chuong trinh sap xep mang tang dan ---\n";
+    cout << "Nhap vao " << N << " phan tu: " << endl;
+    for (int i = 0; i < N; i++) {
+        cout << "Phan tu [" << i << "]: ";
+        cin >> day[i];
+    }
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (day[j] < day[i]) {
+                swap(day[i], day[j]);
+            }
+        }
+    }
+    cout << "\nDay sau khi sap xep tang dan: " << endl;
+    for (int i = 0; i < N; i++) {
+        cout << day[i] << " ";
+    }
+    cout << endl;
 }
